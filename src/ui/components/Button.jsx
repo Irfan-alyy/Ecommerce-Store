721d938899813b6aaddeba4d224b760b046713cd
irfan-alyy <irfanserboon562@gmail.com>
2025-04-12 02:08:55 +0500

    Product page completed, includes product, description and review section

--- src/ui/components/Button.jsx
+++ src/ui/components/Button.jsx
@@ -1,7 +1,7 @@
 import "./ButtonStyle.css"
 const BasicButton = ({text}) => {
     return (<>
-    <button className="shop-btn px-15 py-4 bg-black text-white text-lg font-semibold  w-fit">
+    <button className="shop-btn px-15 py-4 bg-black text-white text-lg font-semibold  cursor-pointer w-fit">
                      {text}
                     </button>
                     </>  );
